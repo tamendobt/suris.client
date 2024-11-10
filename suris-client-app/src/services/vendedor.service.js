c6141efcc0f2d119415219347cafa250e69ba6b8
amendo <amendo1964@gmail.com>
2024-11-09 22:33:04 -0300

    vendedorService modification

--- suris-client-app/src/services/vendedor.service.js
+++ suris-client-app/src/services/vendedor.service.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const getVendedores = async () =>
 {
-   return  await axios.get("https://run.mocky.io/v3/f86ed577-ebb6-45f2-b427-6dfee334966c").then(response=> response.data)
+   return  await axios.get("https://run.mocky.io/v3/c70569d3-9e72-48fb-8e77-3b8b00ceb0bc").then(response=> response.data)
     .catch(err => {
         console.log(err);
         return[];
